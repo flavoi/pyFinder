abc7ba043f5df26357ccf1f84c4ae540588b98e2
flavoi <flavius476@gmail.com>
2014-08-31 17:47:10 +0200

    Sviluppo funzionalità di aggiunta nuovo PG.

--- pyfinder/exp/caller.py
+++ pyfinder/exp/caller.py
@@ -9,9 +9,61 @@
     @author: Flavio Marcato
 """
 
+import sys
+
+from pyfinder.exp.config import PersonaggioGiocante
+
+"""
+    Inizializza personaggi di livello 1 con gli attributi
+    di base.
+"""
+def crea_nuovo_personaggio():
+    nome_giocatore = raw_input("Inserisci il nome del giocatore: ")
+    nome_personaggio = raw_input("Inserisci il nome del personaggio: ")
+    personaggio = PersonaggioGiocante(nome_giocatore, nome_personaggio)
+    return personaggio
+
+"""
+    Assegna i punti esperienza equamente tra tutti
+    i partecipanti.
+"""
+def divisione_comune():
+	pass
+
+
+"""
+    Invoca il menu principale sulle funzionalita` supportate.
+"""
+def menu_start():
+    ans = True
+    while ans:
+        print ("""
+        1. Crea un nuovo personaggio
+        2. Stampa tutti i personaggi
+        e. Esci
+        """)
+        ans=raw_input("Quale attivita` vuoi fare? ") 
+        
+        if ans == "1": 
+            pg = crea_nuovo_personaggio()
+            print("\nPersonaggio %s creato con successo." % pg)
+        
+        elif ans == "2":
+            print "Funzionalita` in sviluppo."
+            pass
+        
+        elif ans == "e":
+            print("\nCiao!") 
+            sys.exit(0)
+        
+        else:
+            print("\nScelta non valida, riprova.") 
+
+
 """ Invocazione programma principale. """
 def main():
-	pass
+    menu_start()
+
 
 if __name__ == '__main__':
     main()
--- pyfinder/exp/config.py
+++ pyfinder/exp/config.py
@@ -2,10 +2,30 @@
 # -*- coding: utf-8 -*-
 
 """
-	Questo script raccoglie le configurazioni
-	per l'app 'exp'.
+    Questo script raccoglie le configurazioni
+    per l'app 'exp'.
 
-	@author: Flavio Marcato
+    @author: Flavio Marcato
 """
 
 
+"""
+    Un personaggio giocante. 
+    Oggetti di questo tipo sono scaricabili in un dizionario
+    tramite l'attributo __dict__.
+"""
+class PersonaggioGiocante:
+
+	# Funzione costruttrice
+    def __init__(self, nome_giocatore, nome_personaggio):
+        self.nome_giocatore = nome_giocatore
+        self.nome_personaggio = nome_personaggio
+        self.punti_esperienza = 0
+
+    # Assegna punti esperienza al giocatore corrente
+    def add_punti_esperienza(punti):
+    	self.punti_esperienza += punti
+
+    # Stampa una rappresentazione minima del personaggio
+    def __str__(self):
+    	return u"%s" % self.nome_personaggio
