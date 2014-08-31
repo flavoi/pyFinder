66b4553c94ee748778870de7588f9f8d0eaf809e
flavoi <flavius476@gmail.com>
2014-08-31 16:58:18 +0200

    Inizializzata app exp.

---
+++ pyfinder/exp/caller.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" 
+    Programma assegnazione punti esperienza. Supporta 
+    varie modalita` di divisione, ma di default ragiona
+    equamente tra tutti i personaggi giocanti.
+
+    @author: Flavio Marcato
+"""
+
+""" Invocazione programma principale. """
+def main():
+	pass
+
+if __name__ == '__main__':
+    main()
---
+++ pyfinder/exp/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+	Questo script raccoglie le configurazioni
+	per l'app 'exp'.
+
+	@author: Flavio Marcato
+"""
+
+
